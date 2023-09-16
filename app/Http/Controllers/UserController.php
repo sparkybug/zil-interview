d72c541fdf35e5ca329c892d0cbb296936e85937
sinach <ukaegbuosinachi15@gmail.com>
2023-09-16 14:11:05 +0100

    updated usercontroller to retrieve users from db

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        // Retrieving users from DB
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
 }
