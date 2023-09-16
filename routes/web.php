e22592bc915649667f84935befbeac236b54ab4d
sinach <ukaegbuosinachi15@gmail.com>
2023-09-16 14:08:44 +0100

    created the route to access user list page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route to access the user list page
+Route::get('/users', 'UserController@index')->name('users.index');
