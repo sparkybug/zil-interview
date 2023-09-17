8040ecb1989a8828a42823e960c0bc2809d05d91
sinach <ukaegbuosinachi15@gmail.com>
2023-09-18 00:35:25 +0100

    I am tired of committing. Github should provide a way to automatically commit changes

---
+++ resources/views/users/trashed.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Soft Deleted Users</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Deleted At</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trashedUsers as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->firstname }} {{ $user->lastname }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->deleted_at }}</td>
+                    <td>
+                        <a href="{{ route('users.restore', $user->id) }}" class="btn btn-primary">Restore</a>
+                        <a href="{{ route('users.delete-permanently', $user->id) }}" class="btn btn-danger">Delete Permanently</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
